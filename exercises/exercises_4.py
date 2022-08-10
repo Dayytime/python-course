3c68076931681ab64601d19495fc020afe542eaa
Majd <93227924+Dayytime@users.noreply.github.com>
2022-08-10 11:38:30 -0400

    Create exercises_4.py

---
+++ exercises/exercises_4.py
@@ -0,0 +1,63 @@
+
+# exercise 1
+# a. Computer Science
+# b. Darwin's
+# c. H2OH2OH2O
+# d.
+
+# exercise 2
+# a.
+print("They\'ll hibernate during the winter.")
+# b.
+print('"Absolutely not," he said.')
+# c.
+print('"He said, \'Absolutely not,\'" recalled Mel.')
+# d.
+print("hydrogen sulfide")
+# e.
+print("left\\right")
+
+# exercise 3
+print("A\nB\nC")
+
+# exercise 4
+wow = ""
+print(len(wow))
+
+# exercise 5
+x = 3
+y = 12.5
+# a.
+print("The rabbit is " + str(x) + ".")
+# b.
+print("The rabbit is " + str(x) + " years old.")
+# c.
+print(str(y) + " is average.")
+# d.
+print(str(y) + " * " + str(x))
+# e.
+print(str(y) + " * " + str(x) + " is " + str(x * y))
+
+# exercise 6
+# Doe, John
+
+# exercise 7
+
+num = float(input("Enter a number: "))
+print(num)
+
+# exercise 8
+
+
+def repeat(s: str, n: int) -> str:
+    return print(s * n)
+
+
+repeat('I don\'t know', 500)
+
+# exercise 9
+
+def total_length(s1: str, s2: str) -> int:
+    return print(len(s1 + s2))
+
+total_length('yes', 'no')
