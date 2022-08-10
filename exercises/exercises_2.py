a900d0ed1a26d5d98581ed79daf8ca69765ba70d
Majd <93227924+Dayytime@users.noreply.github.com>
2022-08-10 11:42:44 -0400

    Create exercises_2.py

---
+++ exercises/exercises_2.py
@@ -0,0 +1,72 @@
+# exercise 1
+# give the values for the following expressions
+
+print(9 - 3)
+# 6
+print(8 * 2.5)
+# 20.0
+print(9 / 2)
+# 4.5
+print(9 / -2)
+# -4.5
+print(9 // -2)
+# -4 ???
+print(9.0 % 2)
+# 1.0
+print(9 % 2.0)
+# 1.0
+print(9 % -2)
+# -1
+print(-9 % 2)
+# 1
+print(9 / -2.0)
+# -4.5
+print(4 + 3 * 5)
+# 19
+print((4 + 3) * 5)
+# 35
+
+# exercise 2
+# what is the output of the following?
+x = -17
+print(+x)
+# the output is -17 because -17 * 1 = -17
+
+# exercise 3
+
+temp = 24
+temp = temp * 1.8 +32
+print(temp)
+
+# exercise 4
+# for each of the following expressions, in which order are the subexpressions evaluated?
+
+# 6 * 3 + 7 * 4
+# * then +
+
+# 5 + 3 / 4
+# / then +
+
+# 5 - 2 * 3 ** 4
+# * then - then **
+
+# exercise 5
+x = 10.5
+y = 4
+x = x + y
+print(x)
+# result is 14.5
+
+# exercise 6
+# write a description of what happens when python evaluates the statement x+=x-x when x has the value of 3
+# python does x - x which is 3 - 3
+# then python runs x+= 0 which gives the output of 3
+# 3 is then assigned to x
+
+# exercise 7
+print(d)
+
+# exercise 8
+# 8 = people (b)
+# 4+= 7/2 (e)
+
